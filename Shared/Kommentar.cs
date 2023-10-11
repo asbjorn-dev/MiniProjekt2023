2386009ae19e4ed4f5b29f23064d7c3479fdb6e2
Asbjørn Knudsen <eaaatkn@students.eaaa.dk>
2023-10-11 11:01:24 +0200

    datamodeller

---
+++ Shared/Kommentar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    public class Kommentar
+    {
+        public int Kommentar_Id { get; set; }
+        public string Tekst { get; set; }
+        public DateTime Dato { get; set; }
+        public int? Antal_Stemmer { get; set; }
+        public int? Upvote { get; set; }
+        public int? Downvote { get; set; }
+
+    }
+}
---
+++ Shared/Tråde.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    public class Tråde
+    {
+        public Tråde(Bruger bruger, Kommentar Kommentar)
+        {
+            this.Bruger = bruger;
+            Kommentare = new List<Kommentar>();
+
+        }
+        public int Tråde_Id { get; set; }
+        public DateTime Dato { get; set; }
+        public string Tråde_Overskrift { get; set; }
+        public string Tekst { get; set; }
+        public int Antal_Stemmer { get; set; }
+        public int Upvote { get; set; }
+        public int Downvote { get; set; }
+        public Bruger Bruger { get; set; }
+        public List<Kommentar> Kommentare { get; set; }
+    }
+}
